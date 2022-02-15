9bd8641b7e8801a7478dd08f08c67fed85a1530e
Jody-septiawan <jody.septiawan5@gmail.com>
2022-02-15 16:58:56 +0900

    Fix ENV

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create base URL API
 export const API = axios.create({
-  baseURL: process.env.SERVER_URL || 'http://localhost:5000/api/v1/',
+  baseURL: process.env.REACT_APP_SERVER_URL || 'http://localhost:5000/api/v1/',
 });
 
 // Set Authorization Token Header
